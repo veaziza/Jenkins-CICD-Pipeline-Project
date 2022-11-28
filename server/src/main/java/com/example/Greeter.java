7cd9203feba4b8584887345684e50c003d587cdf
Vincent Aziza <veaziza@yahoo.com>
2022-11-28 09:40:19 -0600

    testing scan

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -16,6 +16,7 @@ public class Greeter {
    * This is a method.
    */
   public final String greet(final String someone) {
+    string DB_PASSWORD = "Admin@12345"
     return String.format("Hello Avinash, %s!", someone);
   }
 }
